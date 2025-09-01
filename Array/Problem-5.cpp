6059bbd91dc79980c60657bc867a67953b071b18
DebugDynamo01 <bhardwajhardik46@gmail.com>
2025-09-01 23:00:33 +0530

    Find missing num from given array

---
+++ Array/Problem-5.cpp
@@ -0,0 +1,23 @@
+// Program to find the missing number from the array which has the number in the range 1 to n.
+
+#include <iostream>
+using namespace std;
+
+int missingNum(int *arr, int n){
+    int total = 10; // Total number of elements from 1 to n.
+    int sum = (total * (total + 1)) / 2; // Sum of first n natural numbers.
+    int arrSum = 0;
+    for(int i = 0; i < n; i++){
+        arrSum += arr[i];
+    }
+    return sum - arrSum;
+}
+
+int main(){
+    int arr[] = {1, 2, 4, 5, 6, 7, 8, 9, 10};
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+    int missingNumber = missingNum(arr, n);
+    cout << "The missing number is: " << missingNumber << endl;
+    return 0; 
+}
